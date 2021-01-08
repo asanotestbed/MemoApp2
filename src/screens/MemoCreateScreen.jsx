0d1a90b17547e5fd63c468e26b65285344b7d2aa
Tetsuya Asano <tetsuyaasano@tetsuyabookpuro.home>
2021-01-08 15:54:26 +0000

    Create MemoEditScreen and MemoCreateScreen

---
+++ src/screens/MemoCreateScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  View, TextInput, StyleSheet,
+} from 'react-native';
+import AppBar from '../components/AppBar';
+import CirculeButton from '../components/CirculeButton';
+import KeyboardSafeView from '../components/KeyboardSafeView';
+
+export default function MemoCreateScreen() {
+  return (
+    <KeyboardSafeView style={styles.container} behavior="height">
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value="" multiline style={styles.input} />
+      </View>
+      <CirculeButton name="check" />
+    </KeyboardSafeView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
