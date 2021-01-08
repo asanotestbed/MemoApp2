3b3936a6940d8e18b046e2cd4016997365536f58
Tetsuya Asano <tetsuyaasano@tetsuyabookpuro.home>
2021-01-08 15:57:57 +0000

    Fix minor error

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -8,7 +8,7 @@ import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoCreateScreen() {
   return (
-    <KeyboardSafeView style={styles.container} behavior="height">
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="" multiline style={styles.input} />
